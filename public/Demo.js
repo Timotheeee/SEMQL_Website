893267d419b16e5b9fc546980eb126d3e2f2c7e0
gabriele.pace@hotmail.com <gabriele.pace@hotmail.com>
2020-09-26 12:43:57 +0200

    Fixed merge conflict

--- public/Demo.js
+++ public/Demo.js
@@ -1,9 +1,8 @@
 "use strict";
 $.ajax({url: "/api/data/", method: "get"}).done(function (dat) {
-    
+
     window.human = [];
     window.gen = [];
-<<<<<<< HEAD
     for (var i = 0; i < dat.data.length; i++) {
         var h = dat.data[i].human;
         var g = dat.data[i].generated;
@@ -20,23 +19,6 @@ $.ajax({url: "/api/data/", method: "get"}).done(function (dat) {
             var found = false;
             for (var j = 0; j < gs.length; j++) {//for every human word check how many generated words match in a row
                 if(i===1)console.log("hs[i2] is " + hs[i2] + ", gs[j] is " + gs[j]);
-=======
-    for (let i = 0; i < dat.data.length; i++) {
-        let h = dat.data[i].human;
-        let g = dat.data[i].generated;
-        let hs = h.split(" ");//human sentences split by space
-        let gs = g.split(" ");
-        let longestStreakCount = 0;
-        let longestStreak = "";
-
-        for (let i3 = 0; i3 < hs.length; i3++) {
-            //console.log("hs is " + hs[i]);
-            let streak = 0;
-            let words = "";
-            let i2 = i3;
-            for (let j = 0; j < gs.length; j++) {//for every human word check how many generated words match in a row
-                //console.log("hs[i2] is " + hs[i2] + ", gs[j] is " + gs[j]);
->>>>>>> Fixed loading Demo from csv file
                 if (gs[j] === hs[i2]) {
                     streak++;
                     words += gs[j] + " ";
@@ -56,13 +38,7 @@ $.ajax({url: "/api/data/", method: "get"}).done(function (dat) {
                 if(found)i2++;
             }
         }
-
         if(i===1)console.log(longestStreak);
-
-
-
-
-
         human.push({text: h.trim(), highlight: longestStreak.trim(), id: i,side:"left"});
         gen.push({text: g.trim(), highlight: longestStreak.trim(), id: i,side:"right"});
         //break;
@@ -72,9 +48,7 @@ $.ajax({url: "/api/data/", method: "get"}).done(function (dat) {
         window.home.update();
     }
 
-
     setTimeout(function () {
-<<<<<<< HEAD
         for (var i = 0; i < window.gen.length; i++) {
             if(i===1){
                 console.log($("#comp" + i + "left").html());
@@ -82,19 +56,9 @@ $.ajax({url: "/api/data/", method: "get"}).done(function (dat) {
             }
             var h = $("#comp" + i + "left").html().replace(window.human[i].highlight, "<span class='highlight'>" + window.human[i].highlight + "</span>");
             var g = $("#comp" + i + "right").html().replace(window.gen[i].highlight, "<span class='highlight'>" + window.gen[i].highlight + "</span>");
-=======
-        for (let i = 0; i < window.gen.length; i++) {
-            let h = $("#comp" + i + "left").html().replace(window.human[i].highlight, "<span class='highlight'>" + window.human[i].highlight + "</span>");
-            let g = $("#comp" + i + "right").html().replace(window.gen[i].highlight, "<span class='highlight'>" + window.gen[i].highlight + "</span>");
->>>>>>> Fixed loading Demo from csv file
 
             $("#comp" + i + "left").html(h);
             $("#comp" + i + "right").html(g);
         }
     }, 1000);
-
-
-
-
-
 });
