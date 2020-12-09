937b98f270c56f31ecea5a918b7eec11cf43929d
Timotheeee <timothe_laborie@yahoo.fr>
2020-12-09 18:05:07 +0100

    added button to remove selects

--- public/Demo.js
+++ public/Demo.js
@@ -2,77 +2,85 @@
 
 
 
-function compare(a, b) {
-    if (a === undefined || b === undefined) return false;
-    a = a.replace(/[^\w\s]/gi, '').toLowerCase();
-    b = b.replace(/[^\w\s]/gi, '').toLowerCase();
-    return a === b;
-}
+//function compare(a, b) {
+//    if (a === undefined || b === undefined)
+//        return false;
+//    a = a.replace(/[^\w\s]/gi, '').toLowerCase();
+//    b = b.replace(/[^\w\s]/gi, '').toLowerCase();
+//    return a === b;
+//}
 
-function contains(highlights, words) {
-    for (var i = 0; i < highlights.length; i++) {
-        //console.log("comparing: " + highlights[i].trim() + ", " + words.trim());
-        if (highlights[i].trim().includes(words.trim())) {
-            //console.log("returning true");
-            return true;
-        }
+//function contains(highlights, words) {
+//    for (var i = 0; i < highlights.length; i++) {
+//        //console.log("comparing: " + highlights[i].trim() + ", " + words.trim());
+//        if (highlights[i].trim().includes(words.trim())) {
+//            //console.log("returning true");
+//            return true;
+//        }
+//
+//    }
+//    return false;
+//}
 
-    }
-    return false;
-}
-
-function compareTexts(i) {
-    console.log(i);
-    console.log(window.dat.data[i]);
-    var h = $("#humanText" + i).html();
-    var g = $("#generatedText" + i).html();
-    var hs = h.split(" ");//human sentences split by space
-    var gs = g.split(" ");
-    var highlights = [];
-
-    for (var i3 = 0; i3 < hs.length; i3++) {
-        //if (i === 0) console.log("hs[i3] is " + hs[i3]);
-        var words = "";
-        var i2 = i3;
-        var found = false;
-        for (var j = 0; j < gs.length; j++) {//for every human word check how many generated words match in a row
-            //if (i === 0) console.log("hs[i2] is " + hs[i2] + ", gs[j] is " + gs[j]);
-            if (compare(gs[j], hs[i2])) {
-                words += gs[j] + " ";
-                found = true;
-            } else {
-                if (i === 0 && !contains(highlights, words)) console.log("pushing words: " + words);
-                if (words.trim().split(" ").length > 1 && !contains(highlights, words)) highlights.push(words);
-                if (found) break;
-            }
-            if (found) i2++;
-        }
-        //if (i === 0) console.log("pushing words: " + words);
-        if (words.trim().split(" ").length > 1 && !contains(highlights, words)) highlights.push(words);
-
-    }
-
-
-
-    for (var i4 = 0; i4 < highlights.length; i4++) {
-        highlights[i4] = highlights[i4].trim();
-    }
-
-    var highlights2 = [];
-    $.each(highlights, function (i, el) {
-        if ($.inArray(el, highlights2) === -1) highlights2.push(el);
-    });
-
-    if (i === 0) console.log(highlights2);
-
-    for (var j = 0; j < highlights2.length; j++) {
-        var h = $("#humanText" + i).html().replace(highlights2[j], "<span class='highlight" + j + "'>" + highlights2[j] + "</span>");
-        var g = $("#generatedText" + i).html().replace(highlights2[j], "<span class='highlight" + j + "'>" + highlights2[j] + "</span>");
-
-        $("#humanText" + i).html(h);
-        $("#generatedText" + i).html(g);
-    }
-}
+//function compareTexts(i) {
+//    console.log(i);
+//    console.log(window.dat.data[i]);
+//    var h = $("#humanText" + i).html();
+//    var g = $("#generatedText" + i).html();
+//    var hs = h.split(" ");//human sentences split by space
+//    var gs = g.split(" ");
+//    var highlights = [];
+//
+//    for (var i3 = 0; i3 < hs.length; i3++) {
+//        //if (i === 0) console.log("hs[i3] is " + hs[i3]);
+//        var words = "";
+//        var i2 = i3;
+//        var found = false;
+//        for (var j = 0; j < gs.length; j++) {//for every human word check how many generated words match in a row
+//            //if (i === 0) console.log("hs[i2] is " + hs[i2] + ", gs[j] is " + gs[j]);
+//            if (compare(gs[j], hs[i2])) {
+//                words += gs[j] + " ";
+//                found = true;
+//            } else {
+//                if (i === 0 && !contains(highlights, words))
+//                    console.log("pushing words: " + words);
+//                if (words.trim().split(" ").length > 1 && !contains(highlights, words))
+//                    highlights.push(words);
+//                if (found)
+//                    break;
+//            }
+//            if (found)
+//                i2++;
+//        }
+//        //if (i === 0) console.log("pushing words: " + words);
+//        if (words.trim().split(" ").length > 1 && !contains(highlights, words))
+//            highlights.push(words);
+//
+//    }
+//
+//
+//
+//    for (var i4 = 0; i4 < highlights.length; i4++) {
+//        highlights[i4] = highlights[i4].trim();
+//    }
+//
+//    var highlights2 = [];
+//    $.each(highlights, function (i, el) {
+//        if ($.inArray(el, highlights2) === -1)
+//            highlights2.push(el);
+//    });
+//
+//    if (i === 0)
+//        console.log(highlights2);
+//
+//    for (var j = 0; j < highlights2.length; j++) {
+//        var h = $("#humanText" + i).html().replace(highlights2[j], "<span class='highlight" + j + "'>" + highlights2[j] + "</span>");
+//        var g = $("#generatedText" + i).html().replace(highlights2[j], "<span class='highlight" + j + "'>" + highlights2[j] + "</span>");
+//
+//        $("#humanText" + i).html(h);
+//        $("#generatedText" + i).html(g);
+//    }
+//}
 
 
 $.ajax({url: "/api/data/", method: "get"}).done(function (dat) {
@@ -110,23 +118,22 @@ $("html").on("click", "#generatedText0 *", function () {
     }, 3000);
 });
 
-function load(id) {
-    console.log("load");
-    console.log(id);
-//    if (id === 0 || id === 1) {
-    var text = $("#humanText" + id).html();
-    console.log(text);
-
-
-
+function removeSelects() {
     $("#select0").html($("#select0 option:selected").text().replace("_", " "));
     $("#select1").html($("#select1 option:selected").text().replace("_", " "));
     $("#select2").html($("#select2 option:selected").text().replace("_", " "));
     $("#select3").html($("#select4 option:selected").text().replace("_", " "));
+}
 
-    //text = text.replace(/<sele.*<\/select>/,select);
-    text = $("#humanText" + id).text();
-    console.log(text);
+function load(id) {
+    var text = $("#humanText" + id).html();
+    if (text.includes("<select name"))
+        removeSelects();
+    text = $("#humanText" + id).html();
+    for (var i = 0; i < 1000; i++) {
+        text = text.replace(/<[^>]+>/, "");
+    }
+    $("#humanText" + id).html(text);
 
 
     $.ajax({url: "http://160.85.252.156:5004/api/text_to_tree?text=" + text.replace(" ", "_"), method: "get"}).done(function (dat2) {
@@ -140,9 +147,6 @@ function load(id) {
         $('#generatedText' + id).html(dat2.synthetic_question === "" ? "error on the backend" : dat2.synthetic_question);
         //console.log("running compare with " + id);
         //compareTexts(id);
-
-
-
     });
     //compareTexts(id);
 //    } else {
@@ -177,13 +181,14 @@ $.getJSON("attributes_for_table.json", function (json) {
 function genSelect(table, def) {
     def = def.replace(" ", "_");
     def = def.endsWith("s") ? def.slice(0, -1) : def;
-    console.log(def);
+    //console.log(def);
     var fields = attributes[table];
 
     var select = "<select name='" + table + "'>";
     for (var i = 0; i < fields.length; i++) {
         var cur = fields[i][0];
-        if (cur === "id") continue;
+        if (cur === "id")
+            continue;
         var sel = cur === def ? "selected='selected'" : "";
         select += "<option value='" + cur + "' " + sel + ">" + cur + "</option>\n";
     }
@@ -192,7 +197,7 @@ function genSelect(table, def) {
 }
 
 function fixit(id) {
-    console.log("RUNNING FIX " + id);
+    //console.log("RUNNING FIX " + id);
     //if (id === 0) {// || id === 1
 
     setTimeout(function () {
